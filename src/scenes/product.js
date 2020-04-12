6261f0c0b8de2742fab93427da1d0bec18540afb
GMontalvo404 <gmontalvo404@gmail.com>
2020-04-12 17:52:49 -0500

    product detail design finished

--- src/scenes/product.js
+++ src/scenes/product.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, Image, Dimensions } from "react-native";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { addToCart, removeItem } from "../actions/cartActions";
+
+const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get('window').width;
 
 class ProductDetail extends Component {
   addToCart = () => {
@@ -16,7 +19,19 @@ class ProductDetail extends Component {
     const { product } = route.params;
     return (
       <View style={styles.container}>
-        <Text style={styles.itemText}> {product.name}</Text>
+        <View style={styles.product}>
+          <Image source={product.image} style={styles.image}/>
+          <View style={styles.productDetail}>
+            <View style={styles.productDesc}>
+              <Text style={styles.productName}> {product.name}</Text>
+              <Text style={styles.productVolume}> {product.volume}</Text>
+              <Text style={styles.productAlcohol}> {product.alcohol}% Alc</Text>
+            </View>
+            <View style={styles.productPriceDesc}>
+              <Text style={styles.productPrice}> ${product.price} </Text>
+            </View>
+          </View>
+        </View>
         <Text>cantidad</Text>
         <Button
           title="Añadir al carrito"
@@ -40,13 +55,62 @@ export default connect(mapStateToProps, mapDispatchToProps)(ProductDetail);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
     backgroundColor: "#ccc",
     justifyContent: "space-around",
     alignItems: "center",
   },
-  itemText: {
-    color: "#fff",
+  product: {
+    alignItems: 'center',
   },
+  image : {
+    height: screenWidth / 1.5,
+    width: screenWidth / 1.5,
+    resizeMode: 'cover',
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+  },
+  productDetail: {
+    width: screenWidth / 1.2,
+    padding: 10,
+    flex: 1,
+    flexDirection: 'row',
+    borderRadius: 8,
+    backgroundColor: '#000'
+  },
+  productDesc: {
+    width: '50%',
+    paddingRight: 10,
+    textAlign: 'right',
+    borderRightWidth: 2,
+    borderRightColor: '#BBBBBB'
+  },
+  productPriceDesc: {
+    width: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  productName: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+  productVolume: {
+    fontSize: 16,
+    color: '#BBBBBB',
+  },
+  productAlcohol: {
+    fontSize: 16,
+    color: '#BBBBBB',
+  },
+  productPrice: {
+    textAlign: 'center',
+    fontSize: 26,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#FFF',
+  },  
   btnAddToCart: {
 
   }
