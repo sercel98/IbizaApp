97a66e925083af8615eab861ad3cee809df49716
cbustillo22 <carlosj99.cjbl@gmail.com>
2020-07-24 23:14:42 -0500

    userForm created.

--- src/scenes/userForm.js
+++ src/scenes/userForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlatList, Button, StyleSheet, Text, View } from "react-native";
+import {FlatList, Button, StyleSheet, Text, View, TextInput} from "react-native";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import CartItemDetail from "../components/cartItemDetail";
@@ -24,7 +24,38 @@ function UserForm(props) {
 
   return (
     <View style={styles.container}>
-      <Text>Cart detail. Current products {cartItems.length}</Text>
+      <View style={styles.tittleContainer}>
+        <Text style={styles.tittleText}>Datos de envio</Text>
+      </View>
+      <View style={styles.fieldsContainer}>
+        <Text style={styles.textInputTitle}>Nombre y Apellido:</Text>
+        <TextInput
+            style={styles.SectionStyle}
+        />
+        <Text style={styles.textInputTitle}>Direccion:</Text>
+        <TextInput
+            style={styles.SectionStyle}
+            autoCapitalize="none"
+        />
+        <Text style={styles.textInputTitle}>Telefono:</Text>
+        <TextInput
+            style={styles.SectionStyle}
+            autoCapitalize="none"
+        />
+        <Text style={styles.textInputTitle}>Email:</Text>
+        <TextInput
+            style={styles.SectionStyle}
+            autoCapitalize="none"
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button
+                title= 'Enviar'
+                color='#FBBD40'
+            />
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
@@ -45,10 +76,54 @@ const mapDispatchToProps = (dispatch) =>
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#000000",
+    justifyContent: "center",
+  },
+  fieldsContainer: {
+    backgroundColor: "#000000",
     alignItems: "center",
     justifyContent: "center",
+  },
+  tittleContainer: {
+    alignItems: "flex-start",
+    marginTop: 10,
+  },
+  tittleText: {
+    textAlign: "left",
+    fontSize: 26,
+    fontWeight: "600",
+    lineHeight: 27,
+    color: '#FFFFFF',
+    marginBottom: 60,
+  },
+  SectionStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderWidth: 0.1,
+    borderColor: '#000',
+    height: 40,
+    width: 340,
+    borderRadius: 5,
+    margin: 10,
+    bottom: 8,
+  },
+  textInputTitle: {
+    width: 340,
+    textAlign: 'left',
+    fontSize: 22,
+    lineHeight: 20,
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    color: '#FFFFFF',
+  },
+  button: {
+    top: 50,
+    width: 300,
+  },
+  buttonContainer: {
+    backgroundColor: "#000000",
   },
 });
 
