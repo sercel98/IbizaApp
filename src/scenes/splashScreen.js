754e07a4a5b67a7d25746bf2366f1a0967732c8f
cbustillo22 <carlosj99.cjbl@gmail.com>
2020-08-11 22:27:33 -0500

    video splash does not work

--- src/scenes/splashScreen.js
+++ src/scenes/splashScreen.js
@@ -6,21 +6,24 @@ import { Asset } from "expo-asset";
 class SplashScreen extends React.Component {
   render() {
     const { video } = Asset.fromModule(
-      require('../../assets/videos/splashVideo.mp4')
+      require("../../assets/videos/splashVideo.mp4")
     ).uri;
     return (
       <View style={styles.backgroundVideo}>
-        <Video
-          source={{ uri: video }}
-          rate={1.0}
-          volume={1.0}
-          isMuted={true}
-          resizeMode="cover"
-          shouldPlay
-          isLooping
-          style={{ width: 300, height: 300 }}
-        />
-        <Text style={{color: 'white'}}>AAAAAAAAAAAAAAAAAAA</Text>
+        <View>
+          <Video
+            source={{ uri: video }}
+            rate={1.0}
+            volume={1.0}
+            isMuted={true}
+            resizeMode="cover"
+            shouldPlay
+            isLooping
+            style={{ width: 300, height: 300 }}
+          />
+        </View>
+
+        <Text style={{ color: "white" }}>AAAAAAAAAAAAAAAAAAA</Text>
       </View>
     );
   }
@@ -29,7 +32,7 @@ class SplashScreen extends React.Component {
 const styles = StyleSheet.create({
   backgroundVideo: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: "black",
   },
 });
 
